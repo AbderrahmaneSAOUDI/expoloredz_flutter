95235573ad1a2f788341f8ed81da6afdf212d68c
Abderrahmane SAOUDI <saoudi.abderrahmane@univ-ghardaia.dz>
2025-06-29 19:30:06 +0100

    v0.1: create splash screen

---
+++ lib/component/constants.dart
@@ -0,0 +1,5 @@
+class AppConstants {
+  static const String appName = 'Explore DZ';
+  static const String logoAsset = 'assets/Asset 1.webp';
+  static const Duration splashDuration = Duration(seconds: 3);
+}
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:expoloredz_flutter/screens/login_screen.dart';
+import 'package:expoloredz_flutter/component/constants.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const LoginScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(AppConstants.logoAsset, height: 120),
+            const SizedBox(height: 20),
+            const Text(AppConstants.appName, 
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+            const SizedBox(height: 40),
+            SizedBox(
+              width: 50,
+              height: 50,
+              child: CircularProgressIndicator(
+                strokeWidth: 4,
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
